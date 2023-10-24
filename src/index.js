4583e58e1934d2f9e8e88baee1e9369b4ac64f7c
BlackGolub <niktarrom100@gmail.com>
2023-10-24 13:57:11 +0300

    Convector 2.0 replacing BrowserRouter with RouterProvider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import './assets/styles/global.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-);
+const router = createHashRouter([
+    {
+      path: "/*",
+      element: <App />,
+    }
+  ]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+      <RouterProvider router={router} />
+  );
+  
